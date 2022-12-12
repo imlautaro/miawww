0437c5686c435d8302e6889630dce425d608b154
Lautaro Pereyra <hi@imlautaro.com>
2022-12-12 15:15:48 -0300

    add brand colours

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,16 @@
+import { colors } from '@unocss/preset-mini'
+
 export default defineNuxtConfig({
 	extends: ['github:imlautaro/oxecore'],
 	modules: ['@nuxtjs/supabase'],
 	srcDir: 'src',
 	unocss: {
+		theme: {
+			colors: {
+				primary: colors!.blue,
+				gray: colors!.slate,
+			},
+		},
 		webFonts: {
 			fonts: {
 				sans: 'Poppins:400,600,700',
